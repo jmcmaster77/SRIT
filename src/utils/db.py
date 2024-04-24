510e8af7664ae97d03903a89ddc065bc48cd028e
Jorge Martin <jmcmaster77@gmail.com>
2024-04-24 11:10:00 -0400

    limpieza datat/db

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,7 +2,8 @@ from pymongo import MongoClient
 from config.config import userdb, passworddb, host, portdb
 
 # MONGO_URI = 'mongodb://jm:15332@localhost:27017'
+# MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_URI = f"mongodb://" + userdb + ":" + \
-    passworddb + "@" + host + ":" + portdb + "?authSource=admin"
+     passworddb + "@" + host + ":" + portdb + "?authSource=admin"
 
 dbcon = MongoClient(MONGO_URI)
